62677fde705154a90857c3b3454918479b7781cf
Poudel0 <poudel0sandesh@gmail.com>
2023-07-29 13:52:06 +0545

    Eroor with Mongo

---
+++ models/file.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const File = mongoose.model("File", fileSchema);
+
+module.exports = File;
