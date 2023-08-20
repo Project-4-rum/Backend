0709bb12603e7d81cfe2ed01eb50cdfbb60b25f8
SunX-1224 <sunilsapkota1224@gmail.com>
2023-08-20 08:35:25 +0545

    bug fixes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
 const mongoose = require('mongoose');
